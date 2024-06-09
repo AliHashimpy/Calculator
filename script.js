008673fab253a0450bb8634e5a12e7047df229a0
Ali Hashim PY <112693187+AliHashimpy@users.noreply.github.com>
2024-06-09 16:04:22 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function numberclick(val) {
+  document.getElementById("screen2").value += val;
+}
+function clearscreen() {
+  document.getElementById("screen2").value = "";
+  document.getElementById("screen1").value = "";
+}
+function back() {
+  var value = document.getElementById("screen2").value;
+  document.getElementById("screen2").value = value.substr(0, value.length - 1);
+}
+function equalclick() {
+  var text = document.getElementById("screen2").value;
+  document.getElementById("screen1").value = text;
+  var result = eval(text);
+  document.getElementById("screen2").value = result;
+}
